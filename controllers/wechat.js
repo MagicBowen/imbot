ebeb569c1e5c9b6496f23a9ae00b7dfe5ab749e2
magicbowen <e.bowen.wang@icloud.com>
2018-09-20 16:20:32 +0800

    send template msg

--- controllers/wechat.js
+++ controllers/wechat.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 const config = require('../config');
+const accessTocken = require('../utils/access-tocken');
 const logger = require('../utils/logger').logger('wechat');
 
-const getOpenId = async (ctx) => {
+async function getOpenId(ctx) {
     try {
         const result = await axios.get('https://api.weixin.qq.com/sns/jscode2session',
             {
@@ -24,6 +25,51 @@ const getOpenId = async (ctx) => {
     }
 }
 
+async function sendTemplateMsg(ctx) {
+    const tocken = await accessTocken.getTocken();
+    const url = 'https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=' + tocken;
+
+    try {
+        const result = await axios.post(url,
+            {
+                params: {
+                    template_id: 'jGlP_HnrwRBot5E0_vJu3Y0J8KFgRFep8AEuQBwxUTg',
+                    page: "index",
+                    form_id: ctx.body.formId,
+                    data: {
+                        keyword1: {
+                            value: "数学"
+                        },
+                        keyword2: {
+                            value: "2015年01月05日"
+                        },
+                        keyword3: {
+                            value: "12:00"
+                        },
+                        keyword4: {
+                            value: "逸夫教学楼"
+                        },
+                        keyword5: {
+                            value: "401"
+                        }
+                    },
+                    emphasis_keyword: "keyword1.DATA",
+                    touser: 'o2Yr80OCnF1rMkmf6ss10aj3CzAw'
+                }
+            }
+        );
+    
+        ctx.response.type = "application/json";
+        ctx.response.status = 200;
+        ctx.response.body = result;
+    } catch(err) {
+        ctx.response.status = 404;
+        logger.error('send template msg error: ' + err);
+    }
+
+}
+
 module.exports = {
-    'GET /openid' : getOpenId
+    'GET /openid' : getOpenId,
+    'POST /template_msg' : sendTemplateMsg
 };
